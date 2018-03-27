d1b3de4f5a83aa1e88701f6ec341862efac9e19b
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-03-26 22:12:30 -0300

    Primeiro commit com os exercícios

---
+++ exercicios/desafioUnicoUsuario.js
@@ -0,0 +1,16 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+bot.start( ctx => {
+    const from = ctx.update.message.from
+
+    if(from.id == env.id) {
+        ctx.reply('Ao seu dispor, mestre!')
+    }
+    else {
+        ctx.reply('Sinto muito, mas eu só falo com o meu mestre!')
+    }
+})
+
+bot.startPolling()
